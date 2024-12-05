b6433c0d2dc44d813e656bc4a834fc7943348b0f
ktk8916 <ktk8916@naver.com>
2024-12-06 02:11:57 +0900

    required 조건 추가

--- src/main/java/com/survey/api/domain/survey/submit/SurveySubmitManager.java
+++ src/main/java/com/survey/api/domain/survey/submit/SurveySubmitManager.java
@@ -1,11 +1,12 @@
 package com.survey.api.domain.survey.submit;
 
 import com.survey.api.domain.survey.item.SurveyItemEntity;
+import com.survey.api.global.ExceptionCode;
+import com.survey.api.global.SurveyAppException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.stream.IntStream;
 
 @Component
 @RequiredArgsConstructor
@@ -15,21 +16,31 @@ public class SurveySubmitManager {
     private final SurveyAnswersAppender surveyAnswersAppender;
 
     public void submit(List<SurveyItemEntity> surveyItems, List<SurveySubmitCommand> surveyAnswerCommands) {
-        if (surveyItems.size() != surveyAnswerCommands.size()) {
-            throw new IllegalArgumentException();
+        if (surveyItems.size() < surveyAnswerCommands.size()) {
+            throw new SurveyAppException(ExceptionCode.INVALID_VALUE);
         }
 
-        List<SurveyAnswerItem> surveyAnswerItems = IntStream.range(0, surveyItems.size())
-                .mapToObj(index -> {
-                    SurveyItemEntity surveyItem = surveyItems.get(index);
-                    SurveySubmitCommand command = surveyAnswerCommands.get(index);
-                    if (surveyItem.getId() != command.surveyItemId()) {
-                        throw new IllegalArgumentException();
-                    }
-                    return surveyAnswerItemFactory.create(surveyItem.getSurveyFormId(), surveyItem.getItemType(), command);
+        // FIXME
+        surveyItems.stream()
+                .filter(
+                        surveyItem -> surveyAnswerCommands.stream().noneMatch(command -> surveyItem.isRequired() && surveyItem.getId().equals(command.surveyItemId()))
+                ).findAny()
+                .ifPresent(surveyItem -> {throw new SurveyAppException(ExceptionCode.INVALID_VALUE, surveyItem.getId()+"");});
+
+        List<SurveyAnswerItem> surveyAnswerItems = surveyAnswerCommands.stream()
+                .map(command -> {
+                    SurveyItemEntity surveyItem = matchingSurveyItem(command, surveyItems);
+                    return surveyAnswerItemFactory.create(surveyItem.getItemType(), surveyItem.getSurveyFormId(), command);
                 })
                 .toList();
 
         surveyAnswersAppender.append(surveyAnswerItems);
+    }
+
+    private SurveyItemEntity matchingSurveyItem(SurveySubmitCommand surveySubmitCommand, List<SurveyItemEntity> surveyItems) {
+        return surveyItems.stream()
+                .filter(surveyItem -> surveyItem.getId() == surveySubmitCommand.surveyItemId())
+                .findAny()
+                .orElseThrow(() -> new SurveyAppException(ExceptionCode.INVALID_VALUE));
     }
 }
